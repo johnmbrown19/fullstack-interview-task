339f7cb6f2ffd3fd10d3662123e7c45b8266394d
John MacLeod Brown <johnmcleodbrown@gmail.com>
2023-04-20 13:28:57 +0100

    Commit 7
    Fixed TypeError
    Made code more functional; implemented Rambda.js

--- admin/src/index.js
+++ admin/src/index.js
@@ -2,10 +2,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const config = require("config");
-const axios = require("axios"); // Replaced 'request' with 'axios'
+const axios = require("axios");
 const fs = require("fs");
-const CsvWriter = require("csv-writer").createObjectCsvWriter;
-const { stringify } = require('csv-stringify');
+const R = require("ramda");
 
 // Instantiate the Express app
 const app = express();
@@ -17,7 +16,7 @@ app.use(bodyParser.json({ limit: "10mb" }));
 app.get("/investments/:id", async (req, res) => {
   try {
     const { id } = req.params;
-    const { data } = await axios.get(`${config.investmentsServiceUrl}/investments/${id}`); // Use 'axios' instead of 'request'
+    const { data } = await axios.get(`${config.investmentsServiceUrl}/investments/${id}`);
     res.send(data);
   } catch (error) {
     console.error(error);
@@ -29,74 +28,50 @@ app.get("/investments/:id", async (req, res) => {
   }
 });
 
+const buildCsvData = (investments, companies) =>
+  investments.map((investment) => {
+    const company = companies.find((c) => c.id === investment.financialCompanyId);
+
+    return {
+      User: investment.userId.replace(/"/g, '""'),
+      FirstName: investment.firstName.replace(/"/g, '""'),
+      LastName: investment.lastName.replace(/"/g, '""'),
+      Date: investment.date.replace(/"/g, '""'),
+      Holding: company ? company.name.replace(/"/g, '""') : "",
+      Value: (investment.investmentTotal * investment.investmentPercentage).toFixed(2),
+    };
+  });
+
+
 app.get("/generate-report", async (req, res) => {
   try {
     const [investmentsResponse, companiesResponse] = await Promise.all([
-      axios.get(`${config.investmentsServiceUrl}/investments`, {
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        }
-      }),
-      axios.get(`${config.financialCompaniesServiceUrl}/companies`, {
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        }
-      })
+      axios.get(`${config.investmentsServiceUrl}/investments`),
+      axios.get(`${config.financialCompaniesServiceUrl}/companies`),
     ]);
 
-    const investments = investmentsResponse.data;
-    const companies = companiesResponse.data;
+    const csvData = buildCsvData(investmentsResponse.data, companiesResponse.data);
 
-    const csvData = investments.map((investment) => {
-      const company = companies.find((c) => c.id === investment.financialCompanyId);
-
-      return {
-        User: investment.userId.replace(/"/g, '""'), // manually escape quote characters
-        FirstName: investment.firstName.replace(/"/g, '""'),
-        LastName: investment.lastName.replace(/"/g, '""'),
-        Date: investment.date.replace(/"/g, '""'),
-        Holding: company ? company.name.replace(/"/g, '""') : "",
-        Value: investment.investmentTotal * investment.investmentPercentage,
-      };
-    });
-
-
-    //  escape any quote characters in the CSV data (w/ CSV-Stringify)
-    const csvContent = stringify(csvData, {
-      header: true,
-      quote: '"',
-      escape: '"',
-      doubleQuote: true,
-      empty: "",
-      columns: [
-        { key: "User", header: "User" },
-        { key: "FirstName", header: "First Name" },
-        { key: "LastName", header: "Last Name" },
-        { key: "Date", header: "Date" },
-        { key: "Holding", header: "Holding" },
-        { key: "Value", header: "Value" },
-      ],
-    });
+    const csvContent = R.join(
+      "\n",
+      R.concat(
+        ['"User","First Name","Last Name","Date","Holding","Value"'],
+        R.map(R.pipe(R.values, R.map(R.concat('"', R.__, '"')), R.join(",")), csvData)
+      )
+    );
 
     const csvFilePath = `${__dirname}/report.csv`;
 
     try {
       await fs.promises.writeFile(csvFilePath, csvContent);
     } catch (error) {
-      console.error(error); // Add this line to log the error
+      console.error(error);
       return res.status(500).send({ message: "An error occurred while saving the report.", error });
     }
 
-
     const csvContentEncoded = encodeURIComponent(csvContent);
 
-    await axios.post(`${config.investmentsServiceUrl}/export`, { csvContentEncoded }, {
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
+    await axios.post(`${config.investmentsServiceUrl}/investments/export`, { csvContentEncoded });
 
     res.status(200).send({ message: "CSV report generated and sent." });
   } catch (error) {
@@ -115,3 +90,4 @@ const server = app.listen(process.env.NODE_ENV === 'test' ? 0 : config.port, (er
 });
 
 module.exports = app;
+
